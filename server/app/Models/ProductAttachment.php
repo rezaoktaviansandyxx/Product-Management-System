3bc4d57945bd0412810032461bee1e46738c779f
Indra Oki Sandy <indraokisandy1@gmail.com>
2025-05-03 22:04:45 +0700

    Delete unused code

--- server/app/Models/ProductAttachment.php
+++ server/app/Models/ProductAttachment.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Traits\UsesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductAttachment extends Model
 {
